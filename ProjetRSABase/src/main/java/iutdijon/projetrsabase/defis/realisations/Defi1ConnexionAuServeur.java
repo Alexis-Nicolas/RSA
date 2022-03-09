d189aecb18028c92e87e8f31e29f744265efc6e5
ewan-r <ewan.rougier@laposte.net>
2022-03-09 08:51:58 +0100

    Classe network fonctionnelle et début du premier défi

--- ProjetRSABase/src/main/java/iutdijon/projetrsabase/defis/realisations/Defi1ConnexionAuServeur.java
+++ ProjetRSABase/src/main/java/iutdijon/projetrsabase/defis/realisations/Defi1ConnexionAuServeur.java
@@ -1,6 +1,7 @@
 package iutdijon.projetrsabase.defis.realisations;
 
 import iutdijon.projetrsabase.defis.Defi;
+import iutdijon.projetrsabase.network.Network;
 import java.io.IOException;
 
 /**
@@ -11,7 +12,21 @@ public class Defi1ConnexionAuServeur extends Defi {
 
     @Override
     public void executer() throws IOException{
-        
+        Network net = new Network();
+        String s = net.receiveMessage();
+        System.out.println(s);
+        int i;
+        s = net.receiveMessage();
+        while(s!="Défi validé"||s!="”Défi échoué !"){
+            System.out.println(s);
+            i = Integer.parseInt(s)+1;
+            System.out.println(i);
+            net.sendMessage(Integer.toString(i));
+            s = net.receiveMessage();
+            System.out.println(s);
+            s = net.receiveMessage();
+        }
+        net.end();
     }
     
 }
--- ProjetRSABase/src/main/java/iutdijon/projetrsabase/network/Network.java
+++ ProjetRSABase/src/main/java/iutdijon/projetrsabase/network/Network.java
@@ -6,7 +6,10 @@
 package iutdijon.projetrsabase.network;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -24,27 +27,32 @@ public class Network {
      * @throws IOException 
      */
     public Network() throws IOException {
-        
+        this.socket = new Socket("127.0.0.1",1234);
+        this.bufr = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+        this.pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream())),true);
     }
     
     /**
      * Envoi d'un message
      */
-    public void sendMessage(String message) {
+    public void sendMessage(String message) throws IOException {
        
+       this.pw.print(message);
+    
     }
         
     /**
      * Réception d'un message
      */
     public String receiveMessage() throws IOException {
-        return null;
+        
+        return this.bufr.readLine();
     }
     
     /**
      * Fin de la connexion
      */
     public void end() throws IOException {
-        
+        this.pw.print("FIN");
     }
 }
