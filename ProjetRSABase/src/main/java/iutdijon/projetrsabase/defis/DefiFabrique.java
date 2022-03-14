ca5a7dd744e7a3b4314644c5e1a8c0131850c5a2
MathisPct <math712b@gmail.com>
2022-03-14 10:53:04 +0100

    fix: implémentation de tous les cas des défis pour éviter les conflits

--- ProjetRSABase/src/main/java/iutdijon/projetrsabase/defis/DefiFabrique.java
+++ ProjetRSABase/src/main/java/iutdijon/projetrsabase/defis/DefiFabrique.java
@@ -18,14 +18,30 @@ public class DefiFabrique {
         
         switch(numeroDuDefis) {
             case 1 : defi = new Defi1ConnexionAuServeur(); break;
+            case 2 : defi = new DefiAddition(); break;
+            case 3 : defi = new Defi3Decalage(); break;
+            case 4 : defi = new DefiSoustraction(); break;
+            case 5 : defi = new Defi5EstInferieur(); break;
+            case 6 : defi = new Defi6EstEgal(); break;
             case 7 : defi = new Defi7EstPair(); break;
             case 8 : defi = new Defi8Multiplication(); break;
-            case 3 : defi = new Defi3Decalage(); break;
-            case 2 : defi = new DefiAddition(); break;
-            case 4 : defi = new DefiSoustraction(); break;
-            case 6 : defi = new Defi6EstEgal(); break;
-            case 11 : defi = new Defi11RandomTailleFixee(); break;
-            case 14 : defi = new Defi14RandomBornes(); break;
+            case 9 : throw new UnsupportedOperationException("Défis non implémenté !"); 
+            case 10: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 11: defi = new Defi11RandomTailleFixee(); break;
+            case 12: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 13: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 14: defi = new Defi14RandomBornes(); break;
+            case 15: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 16: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 17: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 18: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 19: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 20: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 21: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 22: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 23: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 24: throw new UnsupportedOperationException("Défis non implémenté !");
+            case 25: throw new UnsupportedOperationException("Défis non implémenté !");
             default : throw new UnsupportedOperationException("Défis non implémenté !");
         }
         
